8c0b42d49f805c62ed4bf5cd8b1a3ef03f4f2a40
Hovhannes Ghurshudyan <zz.hovo@gmail.com>
2019-06-06 12:21:53 +0400

    Feature/timer (#3)
    * Added timer functionality

--- model/class-btm-timer.php
+++ model/class-btm-timer.php
@@ -10,27 +10,77 @@ class BTM_Timer{
 	const STATE_STOPPED = 'stopped';
 
 	/**
+	 * The timer initial state
+	 *
 	 * @var string
 	 */
 	protected $state = self::STATE_INITIAL;
-
+	/**
+	 * Return the state of timer
+	 *
+	 * @return string
+	 */
 	public function get_state(){
-		throw new Exception('not implemented');
+		return $this->state;
 	}
 
-	public function __construct() {
-		throw new Exception('not implemented');
-	}
+	/**
+	 * Timer start timestamp
+	 *
+	 * @var int
+	 */
+	public $started;
+	/**
+	 * Timer stop timestamp
+	 *
+	 * @var int
+	 */
+	public $stopped;
 
+	/**
+	 * BTM_Timer constructor.
+	 */
+	public function __construct() {}
+
+	/**
+	 * Timer start
+	 *
+	 * @throws Exception
+	 *      in the case the timer is already started
+	 */
 	public function start(){
-		throw new Exception('not implemented');
+		if( $this->get_state() === BTM_Timer::STATE_STARTED ){
+			throw new Exception("Timer is already started.");
+		}else{
+			$this->started = time();
+			$this->state = self::STATE_STARTED;
+		}
 	}
-
+	/**
+	 * Timer stop
+	 *
+	 * @throws Exception
+	 *      in the case the timer is not started
+	 */
 	public function stop(){
-		throw new Exception('not implemented');
+		if( $this->get_state() === BTM_Timer::STATE_STARTED ){
+			$this->stopped = time();
+			$this->state = self::STATE_STOPPED;
+		}else{
+			throw new Exception("Timer is not started.");
+		}
 	}
 
-	public function get_time_elapsed(){
-		throw new Exception('not implemented');
+	/**
+	 * Get the time elapsed after the timer is started
+	 *
+	 * @return int
+	 */
+	public function get_time_elapsed_in_seconds(){
+		if( $this->get_state() === BTM_Timer::STATE_STOPPED ){
+			return $this->stopped - $this->started;
+		}else{
+			return time() - $this->started;
+		}
 	}
 }
