a22b51b1bc13f4ce5eb88a68453b7704e85427fd
hovo <hovo@gitlab.novembit.com>
2019-06-19 13:36:32 +0400

    added functionality to get all the run statuses

--- model/enum/class-btm-task-run-status.php
+++ model/enum/class-btm-task-run-status.php
@@ -15,14 +15,27 @@ final class BTM_Task_Run_Status{
 	const STATUS_IN_PROGRESS = 'in_progress';
 
 	/**
-	 * @var string
+	 * @return array<string,string>  status value and display name
 	 */
-	private $status;
+	public static function get_statuses(){
+		$statuses = array();
+		$reflectionClass = new ReflectionClass( __CLASS__ );
+		foreach ( $reflectionClass->getConstants() as $constant_name => $constant_val ) {
+			if ( 0 === strpos( $constant_name, 'STATUS_' ) ) {
+				$statuses[ $constant_val ] = self::get_status_display_name( $constant_val );
+			}
+		}
+
+		return $statuses;
+	}
+
 	/**
+	 * @param string $status
+	 *
 	 * @return string
 	 */
-	public function get_display_name(){
-		switch ( $this->status ){
+	public static function get_status_display_name( $status ){
+		switch ( $status ){
 			case self::STATUS_REGISTERED :
 				return __( 'Registered', 'background_task_manager' );
 			case self::STATUS_FAILED :
@@ -35,6 +48,17 @@ final class BTM_Task_Run_Status{
 				return __( 'In progress', 'background_task_manager' );
 		}
 		return '';
+	}
+
+	/**
+	 * @var string
+	 */
+	private $status;
+	/**
+	 * @return string
+	 */
+	public function get_display_name(){
+		return self::get_status_display_name( $this->status );
 	}
 	/**
 	 * @return string
