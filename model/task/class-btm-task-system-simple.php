1ac1258f75d7c2f64d31d40d383d247a7d7e91b9
hovo <hovo@gitlab.novembit.com>
2019-07-17 16:06:02 +0400

    added system simple task type

--- model/task/class-btm-task-system-simple.php
+++ model/task/class-btm-task-system-simple.php
@@ -4,6 +4,6 @@ if ( ! defined( 'BTM_PLUGIN_ACTIVE' ) ) {
 	exit;
 }
 
-class BTM_Task_Bulk_Argument_Normalizer extends BTM_Task_System_Simple{
+class BTM_Task_System_Simple extends BTM_Task_Simple{
 	protected $is_system = true;
 }
