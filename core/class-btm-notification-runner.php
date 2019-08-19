8b0ba642af0833b443a80110cfd742812e1f9eeb
Erik <eriktadevosyan07@gmail.com>
2019-08-19 15:19:25 +0400

    bug fix in curl body

--- core/class-btm-notification-runner.php
+++ core/class-btm-notification-runner.php
@@ -11,58 +11,6 @@ if ( ! defined( 'BTM_PLUGIN_ACTIVE' ) ) {
 class BTM_Notification_Runner {
 
 	/**
-	 * BTM_Notification_Runner constructor.
-	 *
-	 * @param I_BTM_Task $task
-	 */
-	public function __construct( $task ) {
-		$this->init_webhook();
-		$callbacks_and_statuses = BTM_Notification_Dao::get_instance()->get_callback_actions_and_statuses();
-		$callback_action = $task->get_callback_action();
-		$status = $task->get_status()->get_value();
-		$this->task_id = $task->get_id();
-		foreach ( $callbacks_and_statuses as $item ){
-			if( $callback_action === $item->callback_action && $status === $item->status ){
-				$this->callback_action = $item->callback_action;
-				$this->status = $item->status;
-				$this->task_url = admin_url( 'admin.php?page=btm-task-view&action=edit&task_id=' . $this->task_id );
-				wp_remote_post(
-					$this->webhook,
-					array(
-						'method'  => 'POST',
-						'headers' => array(
-							'Content-Type'  => 'application/json',
-						),
-						'body' => array(
-							'content' => 'Task #' . $this->task_id . ' - ' . $this->status,
-							'embeds'  => array(
-								'fields' => array(
-									array(
-										"name"=> "Callback action",
-										"value"=> $this->callback_action,
-										"inline"=> true
-									),
-									array(
-										"name"=> "Status",
-										"value"=> $this->status,
-										"inline"=> true
-									),
-									array(
-										"name"=> "Task url",
-										"value"=> $this->task_url,
-										"inline"=> false
-									)
-								)
-							)
-						),
-					)
-				);
-			}
-		}
-	}
-
-
-	/**
 	 * @var $webhook string
 	 */
 	private $webhook = null;
@@ -88,12 +36,71 @@ class BTM_Notification_Runner {
 	private $task_id = null;
 
 	/**
+	 * BTM_Notification_Runner constructor.
+	 *
+	 * @param I_BTM_Task $task
+	 */
+	public function __construct( $task ) {
+		$this->init_webhook();
+		$callbacks_and_statuses = BTM_Notification_Dao::get_instance()->get_callback_actions_and_statuses();
+		$callback_action        = $task->get_callback_action();
+		$status                 = $task->get_status()->get_value();
+		$this->task_id          = $task->get_id();
+		foreach ( $callbacks_and_statuses as $item ) {
+			if ( $callback_action === $item->callback_action && $status === $item->status ) {
+				$this->callback_action = $item->callback_action;
+				$this->status          = $item->status;
+				$this->task_url        = admin_url( 'admin.php?page=btm-task-view&action=edit&task_id=' . $this->task_id );
+				$body                  = json_encode(
+					array(
+						'content' => 'Task #' . $this->task_id . ' - ' . $this->status,
+						'embeds'  => array(
+							array(
+								'fields' => array(
+									array(
+										"name"   => "Callback action",
+										"value"  => $this->callback_action,
+										"inline" => true
+									),
+									array(
+										"name"   => "Status",
+										"value"  => $this->status,
+										"inline" => true
+									),
+									array(
+										"name"   => "Task url",
+										"value"  => $this->task_url,
+										"inline" => false
+									)
+								)
+							)
+						)
+					)
+				);
+
+				$asd = wp_remote_post(
+					$this->webhook,
+					array(
+						'method'  => 'POST',
+						'headers' => array(
+							'Content-Type' => 'application/json',
+						),
+						'body'    => $body
+					)
+				);
+				var_dump( $asd );
+				die;
+			}
+		}
+	}
+
+	/**
 	 * Initialize Discord webhook
 	 */
-	private function init_webhook(){
-		if( BTM_Plugin_Options::get_instance()->get_discord_webhook() !== '' ){
+	private function init_webhook() {
+		if ( BTM_Plugin_Options::get_instance()->get_discord_webhook() !== '' ) {
 			$this->webhook = BTM_Plugin_Options::get_instance()->get_discord_webhook();
 		}
-    }
+	}
 
 }
